ecdda3605e7cb09c368a17d104e8fce46e4731f9
Doumbe-Henry-Larousse <doumbehenrylarousse@gmail.com>
2025-08-15 07:45:16 +0200

    ajout de fichier app.js

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+// Importer tous tes fichiers JS publics
+import '../../public/js/app.js';
+import '../../public/js/appear.js';
+import '../../public/js/bootstrap.js';
+import '../../public/js/bootstrap.min.js';
+import '../../public/js/bxslider.js';
+import '../../public/js/gsap.min.js';
+import '../../public/js/jquery-ui.js';
+import '../../public/js/jquery.fancybox.js';
+import '../../public/js/jquery.form.min.js';
+import '../../public/js/jquery.js';
+import '../../public/js/jquery.validate.min.js';
+import '../../public/js/main-slider-script.js';
+import '../../public/js/mixitup.js';
+import '../../public/js/odometer.js';
+import '../../public/js/owl.js';
+import '../../public/js/popper.min.js';
+import '../../public/js/script.js';
+import '../../public/js/ScrollTrigger.min.js';
+import '../../public/js/select2';
+import '../../public/js/splitType.js';
+import '../../public/js/wow.js';
+
